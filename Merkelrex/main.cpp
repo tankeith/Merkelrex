a61454f317483570d44defc606fbe85cf06d848a
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-10-13 23:04:30 +0800

    Initial Commit

---
+++ Merkelrex/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Merkelrex
+//
+//  Created by Keith Tan on 13/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
