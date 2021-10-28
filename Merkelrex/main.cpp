5ced8f32ad16e592cb31beb97dcd594a2fd1f2d9
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-10-28 09:11:32 +0800

    defined, initialised and used OrderBookEntry class

--- Merkelrex/main.cpp
+++ Merkelrex/main.cpp
@@ -9,6 +9,31 @@
 #include <string>
 #include <vector>
 
+enum class OrderBookType{bid, ask};
+
+class OrderBookEntry
+{
+    public:
+        OrderBookEntry(double _price,
+                       double _amount,
+                       std::string _timestamp,
+                       std::string _product,
+                       OrderBookType _orderType)
+        : price(_price),
+          amount(_amount),
+          timestamp(_timestamp),
+          product(_product),
+          orderType(_orderType)
+        {
+
+        }
+        double price;
+        double amount;
+        std::string timestamp;
+        std::string product;
+        OrderBookType orderType;
+};
+
 void printMenu()
 {
     // 1 print help
@@ -101,21 +126,21 @@ void processUserOption(int userOption)
 
 int main(int argc, const char * argv[]) {
 
-    enum class OrderBookType{bid, ask};
-
-    std::vector<double> prices;
-    std::vector<double> amounts;
-    std::vector<std::string> timestamps;
-    std::vector<std::string> products;
-    std::vector<OrderBookType> orderTypes;
-
-    prices.push_back(5000.01);
-    amounts.push_back(0.001);
-    timestamps.push_back("2020/03/17 17:01:24.884492");
-    timestamps.push_back("ETH/BTC");
-    orderTypes.push_back(OrderBookType::bid);
+//    std::vector<double> prices;
+//    std::vector<double> amounts;
+//    std::vector<std::string> timestamps;
+//    std::vector<std::string> products;
+//    std::vector<OrderBookType> orderTypes;
+//
+//    prices.push_back(5000.01);
+//    amounts.push_back(0.001);
+//    timestamps.push_back("2020/03/17 17:01:24.884492");
+//    timestamps.push_back("ETH/BTC");
+//    orderTypes.push_back(OrderBookType::bid);
+//
+//    std::cout << "prices: " << prices[0] << std::endl;
+//    std::cout << "sizeof unsigned short " << sizeof(unsigned short) << std::endl;
     
-    std::cout << "prices: " << prices[0] << std::endl;
 //
 //    while (true)
 //    {
@@ -123,5 +148,63 @@ int main(int argc, const char * argv[]) {
 //        int userOption = getUserOption();
 //        processUserOption(userOption);
 //    }
+    
+    std::vector<OrderBookEntry> orders;
+
+    orders.push_back( OrderBookEntry{10000,
+                        0.002,
+                        "2020/03/17 17:01:24.884492",
+                        "BTC/USDT",
+                        OrderBookType::bid}
+                                     );
+    
+    orders.push_back( OrderBookEntry{5000,
+                        0.002,
+                        "2020/03/17 17:01:24.884492",
+                        "BTC/USDT",
+                        OrderBookType::bid}
+                                     );
+    
+//    OrderBookEntry order1{10000,
+//                          0.002,
+//                          "2020/03/17 17:01:24.884492",
+//                          "BTC/USDT",
+//                          OrderBookType::bid}; // creates an object
+
+    
+//    order1.price = 100000;
+//    order1.amount = 0.002;
+//    order1.timestamp = "2020/03/17 17:01:24.884492";
+//    order1.product = "BTC/USDT";
+//    order1.orderType = OrderBookType::bid;
+    
+    // 2. Iterate by reference (access original version in the vector)
+    // Iterate over all the elements in orders
+    for (OrderBookEntry& order : orders)
+    {
+        std::cout << "The price is " << order.price << std::endl; // work on original version
+    }
+    
+    // 2. Classic array syntax
+    for (unsigned int i = 0; i < orders.size(); ++i)
+    {
+        std::cout << "The price is " << orders[i].price << std::endl; // work on original version
+    }
+    
+    // 3. Object style syntax
+    for (unsigned int i = 0; i < orders.size(); ++i)
+    {
+        std::cout << "The price is " << orders.at(i).price << std::endl;
+    }
+    
+    
+//    std::cout << "The price is " << orders[0].price << std::endl;
+//    std::cout << "The price is " << orders[1].price << std::endl;
+    
+//    std::cout << "The amount is " << orders[].amount << std::endl;
+//    std::cout << "The timestamp is " << order1.timestamp << std::endl;
+//    std::cout << "The product is " << order1.product << std::endl;
+//    std::cout << "The order type is " << static_cast<std::underlying_type<OrderBookType>::type>(order1.orderType) << std::endl;
+    
     return 0;
 }
