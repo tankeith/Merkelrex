96639dc3253ad663985fef68e7e06544cf97c87b
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-12-06 23:28:08 +0800

    created OrderBook.cpp and .hpp

---
+++ Merkelrex/OrderBook.cpp
@@ -0,0 +1,8 @@
+//
+//  OrderBook.cpp
+//  Merkelrex
+//
+//  Created by Keith Tan on 6/12/21.
+//
+
+#include "OrderBook.hpp"
---
+++ Merkelrex/OrderBook.hpp
@@ -0,0 +1,13 @@
+//
+//  OrderBook.hpp
+//  Merkelrex
+//
+//  Created by Keith Tan on 6/12/21.
+//
+
+#ifndef OrderBook_hpp
+#define OrderBook_hpp
+
+#include <stdio.h>
+
+#endif /* OrderBook_hpp */
