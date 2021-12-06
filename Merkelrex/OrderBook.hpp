f7f3722015c1a29be082e58672780f2d2487e880
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-12-06 23:37:02 +0800

    code for OrderBook.hpp

--- Merkelrex/OrderBook.hpp
+++ Merkelrex/OrderBook.hpp
@@ -5,9 +5,21 @@
 //  Created by Keith Tan on 6/12/21.
 //
 
-#ifndef OrderBook_hpp
-#define OrderBook_hpp
+#pragma once
+#include "OrderBookEntry.hpp"
+#include "CSVReader.hpp"
+#include <string>
+#include <vector>
 
-#include <stdio.h>
-
-#endif /* OrderBook_hpp */
+class OrderBook
+{
+public:
+    /** construct, reading a csv data file */
+    OrderBook(std::string filename);
+    /** return vector of all known products in the dataset*/
+    std::vector<std::string> getKnownProducts();
+    /** return vector of Orders acording to the sent filters*/
+    std::vector<OrderBookEntry> getOrders(OrderBookType type,
+                                          std::string product,
+                                          std::string timestamp);
+};
