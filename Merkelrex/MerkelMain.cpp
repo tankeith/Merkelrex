ff27f8af8813572f476934eb86e284d890d3f519
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-12-08 22:05:28 +0800

    filled out MerkelMain::enterBid

--- Merkelrex/MerkelMain.cpp
+++ Merkelrex/MerkelMain.cpp
@@ -139,12 +139,46 @@ void MerkelMain::enterAsk()
 
 void MerkelMain::enterBid()
 {
-    std::cout << "Make a bid - enter the amount" << std::endl;
+    std::cout << "Make a bid - enter the amount: product, price amount, e.g. ETH/BTC,200,0.5" << std::endl;
+    std::string input;
+
+    std::getline(std::cin, input);
+    
+    std::vector<std::string> tokens = CSVReader::tokenise(input, ',');
+    if (tokens.size() != 3)
+    {
+        std::cout << "MerkelMain::enterBid Bad input! " << input << std::endl;
+    }
+    else
+    {
+        try
+        {
+            OrderBookEntry obe = CSVReader::stringsToOBE(tokens[1],
+                                                         tokens[2],
+                                                         currentTime,
+                                                         tokens[0],
+                                                         OrderBookType::bid);
+            if (wallet.canFulfillOrder(obe))
+            {
+                std::cout << "Wallet looks good. " << std::endl;
+                orderBook.insertOrder(obe);
+            }
+            else
+            {
+                std::cout << "Wallet has insufficient funds. " << std::endl;
+            }
+        } catch (const std::exception& e)
+        {
+            std::cout << "MerkelMain::enterBid Bad input " << std::endl;
+        }
+        
+    }
+    std::cout << "You typed: " << input << std::endl;
 }
 
 void MerkelMain::printWallet()
 {
-    std::cout << "Your wallet is empty" << std::endl;
+//    std::cout << "Your wallet is empty" << std::endl;
     std::cout << wallet.toString() << std::endl;
 }
 
